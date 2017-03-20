ce7157b0ab588c80fd8738301393a4ae8dd6fd91
Unknown <gorgue@correo.ugr.es>
2017-03-20 23:27:21 +0100

    working on localsearch2

--- include/localsearch.h
+++ include/localsearch.h
@@ -7,9 +7,29 @@ using namespace std;
 
 class LocalSearch{
   private:
-    Solution inicial;
+    Solution initial;
     Solution actual;
+
+    std::vector<bool> dlb;
+
+    int iteration;
+    int maxIterations;
+
+    Problem * problem;
   public:
-    
+    LocalSearch();
+    ~LocalSearch();
+
+    void setInitialSolution(Solution &i);
+    Solution& getInitialSolution();
+
+    Solution& getActualSolution();
+
+    void setProblem(Problem &p){problem=&p;}
+    void clearProblem(){problem=0;}
+    Problem& getProblem(){return *problem;}
+
+    void execute();
+
 
 }

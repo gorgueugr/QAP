1e394a076c2f6be3c2ce979ad6f775ea44da8219
Jorge <gorgue@correo.ugr.es>
2017-04-10 17:16:45 +0200

    Testing bestones

--- src/memetic.cpp
+++ src/memetic.cpp
@@ -1,19 +1,26 @@
 #include "memetic.h"
 
-Solution * Memetic::getBestOnes(){
+Solution ** Memetic::getBestOnes(){
   int size=(int)gen.getPopulationSize()*percent;
-  Solution *s=new Solution[size];
+  Solution **s = new * Solution[size];
   int min=INT_MAX;
   int pos=0;
+  //copia de los k primeros elementos, guardamos el menor y su posición
   for(int i=0;i<size;i++){
-    s[i]=gen.getPopulation()[i];
-    if(s[i].cost)
+    s[i]=&gen.getPopulation()[i];
+    if(s[i]->cost <= min){ min = s[i].cost; pos=i;}
   }
-
+  //Recorrer los restantes y si son mayores qe el menor se sustituye y se busca le menor de nuevo
   for(int i=size;i<gen.getPopulationSize();i++){
-
+    if(gen.getPopulation()[i].cost => min){
+        s[pos] = &gen.getPopulation()[i];
+        min=INT_MAX;
+        for(int j=0;j<size;j++){
+          if(s[j]->cost <= min){ min = s[i].cost; pos=i;}
+        }
+    }
   }
-
+  return s;
 }
 
 
@@ -23,17 +30,17 @@ void Memetic::executeGenerationalPMX(){
   gen.setNumPopulation(50);
   gen.generatePopulation();
   gen.setMaxIterations(generations);
-  Solution * s;
+  Solution ** s;
   lb.setMaxIterations(400);
   for(int i=0;i<500;i++){
     gen.executeGenerationalPMX();
-    s=gen.getPopulation();
-    if(best) s=gen.getBestOnes();
+    s=&gen.getPopulation();
+    if(best) s=&gen.getBestOnes();
     for(int j=0;j<gen.getPopulationSize()*percent;j++){
-      lb.setInitialSolution(s[j]);
+      lb.setInitialSolution(*s[j]);
       lb.execute();
-      s[j]=lb.getActualSolution();
-      cout << "\e[A" << "iteration: " << j << endl;
+      s[j]=&lb.getActualSolution();
+      //cout << "\e[A" << "iteration: " << j << endl;
     }
   }
 
@@ -46,16 +53,17 @@ void Memetic::executeGenerationalOrder(){
   gen.setNumPopulation(50);
   gen.generatePopulation();
   gen.setMaxIterations(generations);
-  Solution * s;
+  Solution ** s;
   lb.setMaxIterations(400);
   for(int i=0;i<500;i++){
     gen.executeGenerationalOrder();
-    s=gen.getPopulation();
-    for(int j=0;j<(int)50*percent;j++){
-      lb.setInitialSolution(s[j]);
+    s=&gen.getPopulation();
+    if(best) s=&gen.getBestOnes();
+    for(int j=0;j<gen.getPopulationSize()*percent;j++){
+      lb.setInitialSolution(*s[j]);
       lb.execute();
-      s[j]=lb.getActualSolution();
-
+      s[j]=&lb.getActualSolution();
+      //cout << "\e[A" << "iteration: " << j << endl;
     }
   }
 
@@ -69,18 +77,20 @@ void Memetic::executeStationaryPMX(){
   gen.setNumPopulation(50);
   gen.generatePopulation();
   gen.setMaxIterations(generations);
-  Solution * s;
+  Solution ** s;
   lb.setMaxIterations(400);
   for(int i=0;i<500;i++){
     gen.executeStationaryPMX();
-    s=gen.getPopulation();
-    for(int j=0;j<(int)50*percent;j++){
-      lb.setInitialSolution(s[j]);
+    s=&gen.getPopulation();
+    if(best) s=&gen.getBestOnes();
+    for(int j=0;j<gen.getPopulationSize()*percent;j++){
+      lb.setInitialSolution(*s[j]);
       lb.execute();
-      s[j]=lb.getActualSolution();
-
+      s[j]=&lb.getActualSolution();
+      //cout << "\e[A" << "iteration: " << j << endl;
     }
   }
+
 
 
 }
@@ -91,17 +101,17 @@ void Memetic::executeStationaryOrder(){
   gen.setNumPopulation(50);
   gen.generatePopulation();
   gen.setMaxIterations(generations);
-  Solution * s;
+  Solution ** s;
   lb.setMaxIterations(400);
   for(int i=0;i<500;i++){
     gen.executeStationaryOrder();
-    s=gen.getPopulation();
-    for(int j=0;j<(int)50*percent;j++){
-      lb.setInitialSolution(s[j]);
+    s=&gen.getPopulation();
+    if(best) s=&gen.getBestOnes();
+    for(int j=0;j<gen.getPopulationSize()*percent;j++){
+      lb.setInitialSolution(*s[j]);
       lb.execute();
-      s[j]=lb.getActualSolution();
-
+      s[j]=&lb.getActualSolution();
+      //cout << "\e[A" << "iteration: " << j << endl;
     }
   }
-
 }
